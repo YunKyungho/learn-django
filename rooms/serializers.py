fd0de59c1746da27914133438007729a0dc45204
YunKyungho <sghj2020s@gmail.com>
2024-07-09 21:18:54 +0900

    add api RoomDetail

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,16 +1,42 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Room, Amenity
-
-
-class RoomSerializer(ModelSerializer):
-
-    class Meta:
-        model = Room
-        fields = "__all__"
+from users.serializers import TinyUserSerializer
+from categories.serializers import CategorySerializer
 
 
 class AmenitySerializer(ModelSerializer):
 
     class Meta:
         model = Amenity
+        fields = (
+            "name",
+            "description",
+        )
+
+
+class RoomListSerializer(ModelSerializer):
+
+    class Meta:
+        model = Room
+        fields = (
+            "id",
+            "name",
+            "country",
+            "city",
+            "price",
+        )
+
+
+class RoomDetailSerializer(ModelSerializer):
+
+    owner = TinyUserSerializer()
+    # 관계가 있는 모델을 연결 시켜서 데이터를 주고 싶을 때
+    amenities = AmenitySerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        model = Room
         fields = "__all__"
+        # depth = 1
+        # 모든 관계를 1 만큼 확장시키라는 의미.
+
