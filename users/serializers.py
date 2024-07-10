0d506ee9bd6bd0927b651bbfcbf95c3fe1f5ebb1
GOHO97 <lp2011s@naver.com>
2024-07-10 23:53:52 +0900

    add api User

--- users/serializers.py
+++ users/serializers.py
@@ -11,3 +11,20 @@ class TinyUserSerializer(ModelSerializer):
             "avatar",
             "username"
         )
+
+
+class PrivateUserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        exclude = (
+            "password",
+            "is_superuser",
+            "is_staff",
+            "id",
+            "is_active",
+            "first_name",
+            "last_name",
+            "groups",
+            "user_permissions",
+        )
